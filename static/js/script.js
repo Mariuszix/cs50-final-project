a5a502b3c3ee26e81e2312d7e57d697ffcd4a3f2
Mariuszix <ce.c.marius@gmail.com>
2020-12-12 16:57:49 +0200

    cs50-final

--- static/js/script.js
+++ static/js/script.js
@@ -17,6 +17,12 @@ const search = document.querySelector("#search-bar");
 //Select the new-entry button
 const newEntry = document.querySelector("#add-entry");
 
+//Activate editing in the table.
+const activate = () => {
+  const editing = new TableCellEditing(document.querySelector("table"));
+  editing.init();
+}
+
 if (newEntry) {
   newEntry.addEventListener("click", () => {
     formElem.classList.remove("is-hidden");
@@ -152,6 +158,7 @@ function htmlActivate(elm) {
       if (rx === password) {
         response = true;
         changeText();
+        activate();
       } else {
         response = false;
         alert("password not good!");
@@ -186,119 +193,9 @@ combineArrays = (first, second) => {
   }, {});
 };
 
-// Inspired and helped by "The Codeholic" youtube channel.
-// class that helps edit cells
-class TableCellEditing {
-  constructor(table) {
-    this.tbody = table.querySelector("tbody");
-  }
-
-  init() {
-    this.tds = this.tbody.querySelectorAll("td");
-    this.tds.forEach((td) => {
-      if (td.getAttribute("contenteditable")) {
-        td.setAttribute("contenteditable", false);
-      } else {
-        td.setAttribute("contenteditable", true);
-      }
-
-      td.addEventListener("click", (ev) => {
-        if (!this.inEditing(td)) {
-          this.startEditing(td);
-        }
-      });
-    });
-  }
-
-  startEditing(td) {
-    const activeTd = this.findEditing();
-    if (activeTd) {
-      this.cancelEditing(activeTd);
-    }
-
-    td.className = "in-editing";
-    td.setAttribute("data-old-value", td.innerHTML);
-    this.createButtonToolbar(td);
-  }
-
-  cancelEditing(td) {
-    td.innerHTML = td.getAttribute("data-old-value");
-    td.classList.remove("in-editing");
-  }
-
-  finishEditing(td) {
-    td.classList.remove("in-editing");
-    const data = {};
-
-    let elm = td.parentElement.children;
-    let id = elm[0].getAttribute("data-entry-id");
-    let name = elm[0].innerHTML;
-    let link = elm[1].querySelector("a").innerHTML;
-    let username = elm[2].querySelector("p").innerText;
-    let hash;
-    //If password was not changed
-    if (elm[3].querySelector(".is-hidden")) {
-      console.log("not changed")
-      hash = elm[3].querySelector(".is-hidden").innerHTML;
-    }
-    //If password was changed
-    else {
-      hash = crypt.encrypt(elm[3].querySelector("#password2").innerHTML);
-      
-    }
-
-    let keys = ["id", "name", "link", "username", "hash"];
-    let values = [id, name, link, username, hash];
-
-    for (let i = 0; i < keys.length; i++) {
-      data[keys[i]] = values[i];
-    }
-
-    const response = makeRequests(data);
-    console.log(response);
-
-    this.removeToolbar(td);
-  }
-
-  inEditing(td) {
-    return td.classList.contains("in-editing");
-  }
-
-  createButtonToolbar(td) {
-    const toolbar = document.createElement("div");
-    toolbar.className = "button-toolbar";
-    toolbar.setAttribute("contenteditable", false);
-
-    toolbar.innerHTML = `
-    <div class="button-wrapper">
-    <button class="btn-save">Save</button>
-    </div>
-    `;
-
-    td.appendChild(toolbar);
-
-    const btnSave = toolbar.querySelector(".btn-save");
-
-    btnSave.addEventListener("click", (ev) => {
-      ev.stopPropagation();
-      this.finishEditing(td);
-    });
-  }
-
-  removeToolbar(td) {
-    const toolbar = td.querySelector(".button-toolbar");
-    toolbar.remove();
-  }
-
-  findEditing() {
-    return Array.prototype.find.call(this.tds, (td) => this.inEditing(td));
-  }
-}
 
 if (document.querySelector("table")) {
-  const editing = new TableCellEditing(document.querySelector("table"));
-  const editingMode = document.querySelector("#edit-mode");
-  editingMode.addEventListener("click", () => {
-    editing.init();
-  });
+  activate();
 }
+
+
